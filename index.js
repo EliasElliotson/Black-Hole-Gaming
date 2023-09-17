b80870b33ad773fd4179a2a5e1be338eac3b1cec
EliasElliotson <136011323+EliasElliotson@users.noreply.github.com>
2023-09-16 21:51:30 -0400

    fix: added function to get item from object by string

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 // Import settings.json
 import settings from "./settings.json";
 
+// Create a function to get data from an object by a string
+Object.byString = function(o, s) {
+  s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+  s = s.replace(/^\./, '');           // strip a leading dot
+  var a = s.split('.');
+  for (var i = 0, n = a.length; i < n; ++i) {
+      var k = a[i];
+      if (k in o) {
+          o = o[k];
+      } else {
+          return;
+      }
+  }
+  return o;
+}
+
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `
