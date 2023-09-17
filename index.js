72130df6ef20a3500b6a092b5146d8d2edfbb0f3
EliasElliotson <136011323+EliasElliotson@users.noreply.github.com>
2023-09-16 20:23:09 -0400

    fix: added font settings

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ appDiv.innerHTML = `
   <meta charset="utf-8">
   <title>`+settings.name+` - Black Hole Gaming</title>
   <link href="style.css" rel="stylesheet" />
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Tilt+Neon&display=swap');
+
+    h1 {
+      font-family: 'Tilt Neon', cursive;
+    }
+    
+    body {
+      background-color: #000000;
+      color: #FFFFFF;
+    }
+  </style>
 </head>
 <body>
 <center><h1>`+settings.name+`</h1></center>
