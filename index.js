ee961625c38f39ee8936d49498602a75462e08ab
EliasElliotson <136011323+EliasElliotson@users.noreply.github.com>
2023-09-16 20:45:37 -0400

    fix: properly formatted page css generation

--- index.js
+++ index.js
@@ -1,43 +1,26 @@
 // Import settings.json
-import settings from './settings.json';
+import settings from "./settings.json";
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML =
-  `
+appDiv.innerHTML = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>` +
-  settings.name +
-  ` - Black Hole Gaming</title>
+  <title>`+settings.name+` - Black Hole Gaming</title>
   <style>
-    @import url('` +
-  settings.font.url +
-  `');
+    @import url('`+settings.font.url+`');
     
     body {
       background-color: #000000;
-      color: ` +
-  settings.font.color +
-  `;
-      font-family: '` +
-  settings.font.name +
-  `', ` +
-  settings.font.family +
-  `, '` +
-  settings.font.alt +
-  `', ` +
-  settings.font.alt_family +
-  `;
+      color: `+settings.font.color+`;
+      font-family: '`+settings.font.name+`', `+settings.font.family+`, '`+settings.font.alt+`', `+settings.font.alt_family+`;
     }
   </style>
 </head>
 <body>
-<center><h1>` +
-  settings.name +
-  `</h1></center>
+<center><h1>`+settings.name+`</h1></center>
 </body>
 </html>
 `;
